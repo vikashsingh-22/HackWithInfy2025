0c3a420fa23424afa6942f062bbb1e127fcf9719
vikash kumar <vikashsingh.cs22@gmail.com>
2025-07-03 11:21:50 +0530

    Buy Two Chocolates

---
+++ GreedyApproach/problem6_Buy_Two_Chocolates.java
@@ -0,0 +1,27 @@
+package GreedyApproach;
+
+import java.util.Arrays;
+//Level :-easy 
+//Leetcode problem Link:- https://leetcode.com/problems/buy-two-chocolates/
+public class problem6_Buy_Two_Chocolates {
+    
+    public static int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        int cost = prices[0] + prices[1];
+
+        if (money >= cost) {
+            return money - cost;
+        } else {
+            return money;
+        }
+    }    
+    
+     public static void main(String[] args) {
+        
+        int[] prices = {1, 2, 2};
+        int money = 3;
+
+        int remaining = buyChoco(prices, money);
+        System.out.println("Remaining money: " + remaining);
+    }
+}
